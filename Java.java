23e14f033d459a59c9c042d10eb6efd9863fe42b
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-10-11 19:44:19 +0200

    adding smt new

--- Java.java
+++ Java.java
@@ -4,5 +4,9 @@ public class Java {
         System.out.println("Java");
         System.out.println("is");
         System.out.println("coming!");
+        System.out.println("with");
+        System.out.println("comments");
+        System.out.println("and");
+        System.out.println("translation");
     }
 }
